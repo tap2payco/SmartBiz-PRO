64ff2985531e572909d4b14eb1c4c00136882763
tap2payco <tap2payco@gmail.com>
2026-01-23 00:06:43 -0800

    fix

--- apps/web/src/app/sw.ts
+++ apps/web/src/app/sw.ts
@@ -6,13 +6,14 @@ import { installSerwist } from "@serwist/sw";
 declare global {
     interface WorkerGlobalScope extends SerwistGlobalConfig {
         __WB_MANIFEST: (PrecacheEntry | string)[] | undefined;
+        __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
     }
 }
 
 declare const self: ServiceWorkerGlobalScope;
 
 installSerwist({
-    precacheEntries: self.__WB_MANIFEST,
+    precacheEntries: self.__WB_MANIFEST || self.__SW_MANIFEST,
     skipWaiting: true,
     clientsClaim: true,
     navigationPreload: true,
